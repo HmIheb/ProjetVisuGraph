65ca6bb34e5b9d14c1c8e96e310bb2b62e4e14ea
Rafiq MAHROUG <12108547@f203-16.ig-edu.univ-paris13.fr>
2024-06-12 12:33:24 +0200

    Ajoute la fonctionnalité de l'exportation des annotations en CSV

--- window.py
+++ window.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import sys
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QTextEdit, QVBoxLayout, 
                              QWidget, QGraphicsView, QGraphicsScene, 
@@ -10,7 +9,7 @@ import networkx as nx
 from exporter import Exporter
 from annotation import Annotation
 
-	# GraphVisualizer pour visualiser les graphes d'entités et de relations
+# GraphVisualizer pour visualiser les graphes d'entités et de relations
 class GraphVisualizer(QGraphicsView):
     def __init__(self):
         super().__init__()
@@ -20,6 +19,7 @@ class GraphVisualizer(QGraphicsView):
     def initUI(self):
         self.scene = QGraphicsScene()
         self.setScene(self.scene)
+      
         
     def wheelEvent(self, event):
         zoom_in_factor = 1.25
@@ -32,7 +32,7 @@ class GraphVisualizer(QGraphicsView):
         
         self.scale(zoom_factor, zoom_factor)
 
-	# MainWindow pour intégrer toutes les fonctionnalités
+# MainWindow pour intégrer toutes les fonctionnalités
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -43,48 +43,51 @@ class MainWindow(QMainWindow):
         self.setGeometry(100, 100, 800, 600)
 
         self.graphVisualizer = GraphVisualizer()
+        
         self.annotation = Annotation()
 
         self.centralWidget = QWidget()
         self.layout = QVBoxLayout()
         
         self.layout.addWidget(self.graphVisualizer)
-
-		# Ajouter les boutons d'importation et d'exportation
+        
+        # Ajouter les boutons d'importation et d'exportation
         self.importButton = QPushButton("Import Ace 2005")
         self.importButton.clicked.connect(self.importText)
-        self.exportImageButton = QPushButton("Export Image")
+        self.exportButton = QPushButton("Export Image")
         
         self.exportCSVButton = QPushButton("Export CSV")
-        self.exportCSVButton.clicked.connect(self.export_to_csv)
+        self.exportCSVButton.clicked.connect(self.export_to_csv)     
         
-
         self.buttonLayout = QHBoxLayout()
         self.buttonLayout.addWidget(self.importButton)
-        self.buttonLayout.addWidget(self.exportImageButton)
+        self.buttonLayout.addWidget(self.exportButton)
         self.buttonLayout.addWidget(self.exportCSVButton)
-        self.layout.addLayout(self.buttonLayout)
 
+        self.layout.addLayout(self.buttonLayout)
+        
         self.centralWidget.setLayout(self.layout)
         self.setCentralWidget(self.centralWidget)
 
+   
+
+    
     def importText(self):
         options = QFileDialog.Options()
         fileName, _ = QFileDialog.getOpenFileName(self, "Import xml File", "", "Xml Files (*.xml);;All Files (*)", options=options)
         if fileName:
             with open(fileName, 'r', encoding='utf-8') as file:
                 text = file.read()
-                # appeler le parser ici
-
+                #appeler le parser ici
 
     def export_to_csv(self):
-	    options = QFileDialog.Options()
-	    fileName, _ = QFileDialog.getSaveFileName(self, "Export CSV", "", "CSV Files (*.csv);;All Files (*)", options=options)
-	    if fileName:
-	        Exporter.export_to_csv(self.annotation.entities, self.annotation.relations, self.annotation.events, fileName)
-	        print("Exportation en CSV réussie.")
-
-
+        options = QFileDialog.Options()
+        fileName, _ = QFileDialog.getSaveFileName(self, "Export CSV", "", "CSV Files (*.csv);;All Files (*)", options=options)
+        if fileName:
+            Exporter.export_to_csv(self.annotation.entities, self.annotation.relations, self.annotation.events, fileName)
+            print("Exportation en CSV réussie.")
+                
+    
 """
 Main qui ouvre une fenetre 
 """
@@ -93,5 +96,3 @@ if __name__ == '__main__':
     mainWindow = MainWindow()
     mainWindow.show()
     sys.exit(app.exec_())
-
-
